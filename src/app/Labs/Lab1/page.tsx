073f4a170c0895e78f9dcffec75f74d51ce653c3
Kevin <tolbert.k@northeastern.edu>
2025-09-19 17:02:29 -0400

    Update page.tsx

--- src/app/Labs/Lab1/page.tsx
+++ src/app/Labs/Lab1/page.tsx
@@ -218,10 +218,10 @@ export default function Lab1() {
         id="wd-textarea"
         cols={30}
         rows={10}
-        defaultValue={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`}
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       />
 
       <h5 id="wd-buttons">Buttons</h5>
