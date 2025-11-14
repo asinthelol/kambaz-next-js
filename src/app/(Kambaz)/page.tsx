bbaf9c13127b89aa0fa138c82a8ac3154f0c7987
Kevin <tolbert.k@northeastern.edu>
2025-11-14 17:28:36 -0500

    Update page.tsx

--- src/app/(Kambaz)/page.tsx
+++ src/app/(Kambaz)/page.tsx
@@ -6,9 +6,39 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import * as courseClient from "./Courses/client";
 
+interface Course {
+  _id: string;
+  name: string;
+  number: string;
+  startDate: string;
+  endDate: string;
+  department: string;
+  credits: number;
+  description: string;
+  author?: string;
+  image?: string;
+}
+
+interface User {
+  _id: string;
+  username: string;
+  password: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  dob?: string;
+  role?: string;
+}
+
+interface RootState {
+  accountReducer: {
+    currentUser: User | null;
+  };
+}
+
 export default function Kambaz() {
-  const [courses, setCourses] = useState<any[]>([]);
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const [courses, setCourses] = useState<Course[]>([]);
+  const { currentUser } = useSelector((state: RootState) => state.accountReducer);
   const fetchCourses = async () => {
     try {
       const courses = await userClient.findMyCourses();
